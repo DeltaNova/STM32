826bcced9ec3196e811e7f3bce6946c99f525b79
Matthew Tunstall <git@deltanova.co.uk>
2016-10-10 10:18:01 +0100

    Code Cleanup
    Signed-off-by: Matthew Tunstall <git@deltanova.co.uk>

--- SerialBufferTest/serial_buffer_test.cpp
+++ SerialBufferTest/serial_buffer_test.cpp
@@ -10,8 +10,6 @@
 // ---------------------
 // http://stackoverflow.com/questions/12543076/usart-receive-interrupt-stm32
 extern "C" void USART1_IRQHandler(void); // Solves Interrupt Problem
-//void ClockSetup();  // Setup the system clock sources
-//void SerialSetup(); // Setup USART1
 void delay(int);    // Simple Delay
 void SerialReceiveEcho(); // Serial Test Function - Echos Received Data
 volatile uint8_t flag_overrun = 0; // Monitors SerialRx Overrun
@@ -27,8 +25,8 @@ volatile struct Buffer serial_rx_buffer {{},0,0};
 // Main - This function is called by the startup code.
 // ---------------------------------------------------
 int main() {
-    ClockSetup();
-    SerialSetup();
+    ClockSetup();       // Setup System & Peripheral Clocks
+    SerialSetup();      // Enable Serial Support - Currently USART1 Specific
 
     // Strings
     uint8_t test_message[] = "Waiting!\n\r"; //Size 10, escape chars 1 byte each
@@ -37,8 +35,6 @@ int main() {
 
     LoadBuffer(&serial_tx_buffer, test_message, 10);
     SerialBufferSend(&serial_tx_buffer);
-
-    __enable_irq();  // Is this needed??
 
     while(1){ // Required to prevent SIGTRAP - Infinite loop.
         // Obtain status of rx buffer, Status 0 = holds data, 1 = empty
@@ -49,34 +45,18 @@ int main() {
     }
 }
 
-
-
-
 void delay(int count){
     // volatile so that the compiler doesn't optimise it out
     volatile int i;
-
-    for (i = 0; i < count; i++)
-    {
+    for (i = 0; i < count; i++){
     }
 }
 
 void USART1_IRQHandler(void){
-
     // USART IRQ Triggered
-    //  Read USART_SR & determine RXNE or ORE
-    //  If overrun, increment flag_overrun, store the data to buffer and return.
-    //  If no overrun read the received data to the buffer and return
-
-    //if (USART1->SR & 0x00000008){  // Read Status register
-    //    // If Overrun detected increment flag (ORE Set)
-    //    flag_overrun = flag_overrun + 1;
-    //}
-
-    // Store byte in buffer
-    if (USART1->SR & 0x00000020){ // Read Status register
-        // If Read Data Register not empty (RXNE Set)
-        uint8_t data = (uint8_t)(USART1->DR & 0xFF); // Read Data resgister, Reads lowest 8bits of 32.
+    if (USART1->SR & 0x00000020){ // Read Status reg, check if RXNE Set
+        // Read Rx Data register, Reads lowest 8bits of 32.
+        uint8_t data = (uint8_t)(USART1->DR & 0xFF);
         bufferWrite(&serial_rx_buffer, data);
     }
 }
