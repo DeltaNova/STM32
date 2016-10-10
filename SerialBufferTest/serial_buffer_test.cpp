e3180aa1c111a657e335bf255f6b048f64b3d508
Matthew Tunstall <git@deltanova.co.uk>
2016-10-10 11:08:26 +0100

    Code Cleanup & Added Comments
    Signed-off-by: Matthew Tunstall <git@deltanova.co.uk>

--- SerialBufferTest/serial_buffer_test.cpp
+++ SerialBufferTest/serial_buffer_test.cpp
@@ -1,18 +1,16 @@
 // STM32F103 Serial Setup & Test with Buffer
 ////////////////////////////////////////////////////////////////////////////////
-#include "clock.h"      // Setup system and peripheral clocks
-#include "buffer.h"
-#include "serial.h"
-#include <stdint.h>                     // uint8_t
-#include "stm32f103xb.h"
+#include "clock.h"       // Setup system and peripheral clocks
+#include "buffer.h"      // Circular Buffers
+#include "serial.h"      // USART1 Setup & Functions
+#include <stdint.h>      // uint8_t
+#include "stm32f103xb.h" // HW Specific Header
 ////////////////////////////////////////////////////////////////////////////////
 // Function Declarations
 // ---------------------
 // http://stackoverflow.com/questions/12543076/usart-receive-interrupt-stm32
-extern "C" void USART1_IRQHandler(void); // Solves Interrupt Problem
-void delay(int);    // Simple Delay
-void SerialReceiveEcho(); // Serial Test Function - Echos Received Data
-volatile uint8_t flag_overrun = 0; // Monitors SerialRx Overrun
+// 'extern "C"' Solves Interrupt Problem of using C++ Compiler for C Code
+extern "C" void USART1_IRQHandler(void);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Buffers
@@ -28,19 +26,22 @@ int main() {
     ClockSetup();       // Setup System & Peripheral Clocks
     SerialSetup();      // Enable Serial Support - Currently USART1 Specific
 
-    // Strings
+    // Strings & Initial Values
     uint8_t test_message[] = "Waiting!\n\r"; //Size 10, escape chars 1 byte each
     uint8_t bufferStatus = 1; // Start as 1 indicating empty buffer.
     uint8_t tmp_byte = 0x00;
 
+    // Send a message to the terminal to indicate program is running.
     LoadBuffer(&serial_tx_buffer, test_message, 10);
     SerialBufferSend(&serial_tx_buffer);
 
-    while(1){ // Required to prevent SIGTRAP - Infinite loop.
+    while(1){ // Program Loop - Checks if data has been received.
+
         // Obtain status of rx buffer, Status 0 = holds data, 1 = empty
         bufferStatus = bufferPeek(&serial_rx_buffer,&tmp_byte);
-        if (bufferStatus == 0){                     // If data in buffer
-            SerialBufferSend(&serial_rx_buffer);    // Transmit buffer content
+
+        if (bufferStatus == 0){                  // If data in buffer...
+            SerialBufferSend(&serial_rx_buffer); // ...transmit buffer contents.
         }
     }
 }
