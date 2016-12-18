95ee310956c7f53cefce00e648830ea40baf3fc8
Matthew Tunstall <git@deltanova.co.uk>
2016-12-18 23:05:20 +0000

    Implement longdelay()
    Signed-off-by: Matthew Tunstall <git@deltanova.co.uk>

--- libraries/delay.cpp
+++ libraries/delay.cpp
@@ -1,8 +1,15 @@
 // delay.cpp
+#include <stdint.h>
 
 void delay(int count){
     // volatile so that the compiler doesn't optimise it out
     volatile int i;
+    for (i = 0; i < count; i++){
+    }
+}
+
+void longdelay(uint32_t count){
+    volatile uint32_t i;
     for (i = 0; i < count; i++){
     }
 }
--- libraries/delay.h
+++ libraries/delay.h
@@ -3,6 +3,7 @@
 #ifndef DELAY_H
 #define DELAY_H
 
-void delay(int count);
 
+void delay(int count);
+void longdelay(uint32_t count);
 #endif // DELAY_H
