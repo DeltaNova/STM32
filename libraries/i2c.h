80dbb3295cb25f56a3f542c4fcaf1efc94959876
Matthew Tunstall <git@deltanova.co.uk>
2018-01-27 01:13:10 +0000

    Update i2c.h to reflect removal of I2CReadMode() code.

--- libraries/i2c.h
+++ libraries/i2c.h
@@ -14,7 +14,6 @@ Status I2CStop();
 Status I2CReadByte(uint8_t SlaveAddr, volatile struct Buffer *i2c_rx_buffer);    // Read 1 Byte
 Status I2CRead2Bytes(uint8_t SlaveAddr, volatile struct Buffer *i2c_rx_buffer);  // Read 2 Bytes
 Status I2CRead3Bytes(uint8_t SlaveAddr, uint8_t NumberBytesToRead, volatile struct Buffer *i2c_rx_buffer);  // Read 3+ Bytes
-//void I2CReadMode(uint8_t SlaveAddr);
 Status I2CReadData(uint8_t NumberBytesToRead, uint8_t slaveAddress, volatile struct Buffer *i2c_rx_buffer);
 void I2CRead(uint8_t NumberBytesToRead, uint8_t slaveAddress);
 
