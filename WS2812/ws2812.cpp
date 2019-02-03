340d02840c0e904f672c5f7137c9e62bd5f220df
Matthew Tunstall <git@deltanova.co.uk>
2019-02-03 22:41:17 +0000

    Move SysTick code to separate file.
    Makes delay_ms() easier to include in other libraries.

--- WS2812/ws2812.cpp
+++ WS2812/ws2812.cpp
@@ -10,6 +10,8 @@
 #include "clock.h"              // Setup system and peripheral clocks
 #include "delay.h"              // Simple Delay Function
 #include "stm32f103xb.h"        // HW Specific Header
+#include "systick.h"            // SysTick Configuration
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Buffers
@@ -55,7 +57,7 @@ static uint8_t colour9[][3] = {PINK, PINK, PINK, PINK, PINK};
 */
 ////////////////////////////////////////////////////////////////////////////////
 // Global Variables
-volatile uint32_t ticks = 0;        // Used for SysTick count down.
+
 volatile uint32_t flash = 0;        // Used for PC13 LED Flash Toggle Interval
 volatile uint32_t colour_change = 6001; // Used LED Colour Change Interval
 
@@ -1042,23 +1044,7 @@ void toggleLed(){
     }
 }
 
-void SysTick_Init(void){
-    // SystemCoreClock/1000     =  1ms
-    // SystemCoreClock/100000   = 10us
-    // SystemCoreClock/1000000  =  1us
-    
-    // Triggering the interupt every 1us is probably excessive unless there is a
-    // specific reason. An interrupt every 1ms should be sufficient.
-    
-    // SysTick_Config() is defined in core_cm3.h
-    // Value of SystemCoreClock is defined in startup file.
-    
-    // Setup and start SysTick
-    while(SysTick_Config(SystemCoreClock/1000) != 0){
-        // One SysTick Should now equal 1ms
-        // This means the interrupt SysTick_Handler() will run every 1ms. 
-    }
-}
+
 
 void SysTick_Handler(void){
     if (ticks != 0){
@@ -1081,18 +1067,5 @@ void SysTick_Handler(void){
     //}
 }
 
-void delay_ms(uint32_t ms){
-    
-    if (ms != 0xFFFFFFFF){
-        // Increment the delay count by one to compensate for the pre-decrement
-        // in the SysTick handler.
-        // The only instance where this will not happen is when the delay has
-        // been set to its maximum value. A 1ms difference over the maximum 
-        // delay is insignificant and is ignored. A 1ms difference will be more 
-        // apparent over shorter delays hence the need for compensation.
-        ++ms;
-    }
-    ticks = ms;
-    while(ticks);
-    
 }
+
---
+++ libraries/systick.cpp
@@ -0,0 +1,38 @@
+// systick.cpp - Basic Systick
+#include <stdint.h>             // Enable fixed width integers
+#include "stm32f103xb.h"        // HW Specific Header
+#include "systick.h"
+
+void SysTick_Init(void){
+    // SystemCoreClock/1000     =  1ms
+    // SystemCoreClock/100000   = 10us
+    // SystemCoreClock/1000000  =  1us
+    
+    // Triggering the interupt every 1us is probably excessive unless there is a
+    // specific reason. An interrupt every 1ms should be sufficient.
+    
+    // SysTick_Config() is defined in core_cm3.h
+    // Value of SystemCoreClock is defined in startup file.
+    
+    // Setup and start SysTick
+    while(SysTick_Config(SystemCoreClock/1000) != 0){
+        // One SysTick Should now equal 1ms
+        // This means the interrupt SysTick_Handler() will run every 1ms. 
+    }
+}
+
+void delay_ms(uint32_t ms){
+    
+    if (ms != 0xFFFFFFFF){
+        // Increment the delay count by one to compensate for the pre-decrement
+        // in the SysTick handler.
+        // The only instance where this will not happen is when the delay has
+        // been set to its maximum value. A 1ms difference over the maximum 
+        // delay is insignificant and is ignored. A 1ms difference will be more 
+        // apparent over shorter delays hence the need for compensation.
+        ++ms;
+    }
+    ticks = ms;
+    while(ticks);
+    
+}
