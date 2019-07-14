d49d1a1a6551271e24265640f3dd6558c0a2ec82
Quantum <git@deltanova.co.uk>
2019-07-14 23:19:34 +0100

    Include stdint.h in count.h to remove compile error.

--- libraries/count.h
+++ libraries/count.h
@@ -1,5 +1,9 @@
 // count.h
 #ifndef COUNT_H
 #define COUNT_H
+
+#include <stdint.h>             // Fixed Width Integers
+
 uint32_t get_upcounting_delta(uint32_t start_count, uint32_t stop_count);
+
 #endif
